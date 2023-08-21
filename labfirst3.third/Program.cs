efeb1004ed37167e8872c7cdfc8be570f8b21a2b
hp <hp@DESKTOP-75BFR6A>
2023-08-21 22:48:16 +0530

    Add project files.

---
+++ labfirst3.third/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labfirst3.third
+{
+    class third
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[5];
+            Console.WriteLine("Enter the element of array : ");
+            for (int i = 0; i < 5; i++) Arr[i] = Convert.ToInt32(Console.ReadLine());
+            int largest = Arr[0];
+            for (int i = 1; i < 5; i++)
+            {
+                if (largest < Arr[i])
+                    largest = Arr[i];
+            }
+            Console.WriteLine("the largest element in array : " + largest);
+            Console.ReadLine();
+        }
+    }
+}
